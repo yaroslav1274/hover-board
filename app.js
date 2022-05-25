386ffda5e6c8eda009ef05c3499fc5ba02fefcf8
yaroslav1274 <78589403+yaroslav1274@users.noreply.github.com>
2022-05-25 16:20:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 500;
+const colors = ['#86009e', '#0affa9', '#00ff11', '#663100', '#ae1919'];
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+
+    square.addEventListener('mouseleave', () => removeColor(square));
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 5px ${color}, 0 0 5px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
